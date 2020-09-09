93cdd7045ca1b856a6962a055e8d482290dbeff7
Sal136 <70862873+Sal136@users.noreply.github.com>
2020-09-09 17:55:36 +0300

    Add files via upload

---
+++ hello5.py
@@ -0,0 +1,22 @@
+# Way One 
+print('Way One:')
+print('')
+print('Hello, World!')
+print('Hello, World!')
+print('Hello, World!')
+print('Hello, World!')
+print('Hello, World!')
+print()
+# Way Two
+print('Way Two:\n')
+for i in range(5):
+    print('Hello, World!')
+print()
+# Way Three
+print('Way Three:\n')
+print('Hello, World!\n'*5) 
+
+
+
+  
+
---
+++ volume.py
@@ -0,0 +1,6 @@
+# buggy program to compute volume of sphere
+pi = 3.14159
+r = 2.0
+volume = 4 / 3 * pi * r**3
+
+print(volume)
